51179699c0e42fbc29b453ed86a699e4460cafa3
Henry Shipper <hshipper@gmail.com>
2020-11-17 20:01:12 -0800

    minimal bot setup

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_read():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+with open('.token_file'), 'r') as tk:
+    token = tk.read()
+
+client.run(token)
