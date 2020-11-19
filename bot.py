fa304cd51d116d813494c02bd71d03f3616b924c
Henry Shipper <hshipper@gmail.com>
2020-11-18 21:03:22 -0800

    test sending author name

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
+'''
+A bot to listen to a discord server and keep track of puns that are made
+'''
+
 import discord
+from discord.ext import commands
 
 client = discord.Client()
 
@@ -16,7 +21,16 @@ async def on_message(message):
     if message.content.startswith("$hello"):
         await message.channel.send("Hello!")
 
+bot = commands.Bot(command_prefix="!")
 
+@bot.command()
+async def print_user(ctx):
+    await ctx.send(ctx.author.name)
+
+bot.add_command(print_user)
+
+# the bot needs a secret token to connect
+# that token is being stored in a separate file for safety purposes
 with open(".token_file", "r") as tk:
     token = tk.read()
 
