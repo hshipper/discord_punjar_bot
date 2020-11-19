d698395e4adbfc1ca385a38a25cdd2c47a3471ec
Henry Shipper <hshipper@gmail.com>
2020-11-18 23:37:01 -0800

    starting cogs

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ A bot to listen to a discord server and keep track of puns that are made
 
 import discord
 from discord.ext import commands
+import json
 
 # client = discord.Client()
 
@@ -25,13 +26,15 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is connected!')
     await bot.change_presence(activity=discord.Game(name='around with tests'))
 
 @bot.command()
-async def pun(ctx, arg):
+async def testpun(ctx, arg):
     await ctx.send(f'{arg} made a terrible pun.')
 
 @bot.command()
@@ -44,8 +47,44 @@ async def members(ctx):
 async def test(ctx):
     print(len(ctx.guild.roles))
 
+
+class RecordPuns(commands.Cog):
+    @commands.Cog.listener()
+    async def on_ready():
+        with open('pun_data.json', 'r') as pun_file:
+            puns = json.load('pun_data.json')
+
+    @commands.command()
+    async def deposit(self, ctx, *, member: discord.Member = None):
+        member = member or ctx.author
+        try:
+            puns[member.id]['count'] += 1
+        except:
+            puns[member.id] = {'count': 1}
+        await ctx.send(f'{member} has made {puns[member.id]['count']} awful jokes.')
+
+    @commands.command()
+    async def subtract(self, ctx, *, member: discord.Member = None)
+        member = member or ctx.author
+        try:
+            puns[member.id]['count'] -= 1
+            await ctx.send(f'{member}\'s last joke wasn\'t that bad. They\'ve made {puns[member.id]['count']} puns.')
+        except:
+            ctx.send(f'{member} hasn\'t made any bad jokes, unbelievably.')
+
+class TrackingPuns(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+
+            
+
+
 # the bot needs a secret token to connect
 # that token is being stored in a separate file for safety purposes
+
+
 with open(".token_file", "r") as tk:
     token = tk.read()
 
