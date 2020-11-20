945f9c227a5db8fb94c26a2fedadffcd9c67281a
Henry Shipper <hshipper@gmail.com>
2020-11-19 21:39:25 -0800

    formatted

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-'''
+"""
 A bot to listen to a discord server and keep track of puns that are made
-'''
+"""
 
 import discord
 from discord.ext import commands
-from google.cloud import firestore
+
+# from google.cloud import firestore
 import json
 
 intents = discord.Intents.default()
@@ -14,13 +15,13 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} is connected!')
-    await bot.change_presence(activity=discord.Game(name='around with tests'))
+    print(f"{bot.user.name} is connected!")
+    await bot.change_presence(activity=discord.Game(name="around with tests"))
 
 
 class RecordPuns(commands.Cog):
     def __init__(self, bot):
-        with open('pun_data.json', 'r') as pun_file:
+        with open("pun_data.json", "r") as pun_file:
             self.puns = json.load(pun_file)
         self.bot = bot
         self._last_punmaker = None
@@ -30,22 +31,28 @@ class RecordPuns(commands.Cog):
         member = member or ctx.author
         self._last_punmaker = member
         try:
-            self.puns[member.id]['count'] += 1
+            self.puns[member.id]["count"] += 1
         except KeyError:
-            self.puns[member.id] = {'count': 1}
-        await ctx.send(f"{member.mention} has made {self.puns[member.id]['count']} \
-            awful jokes.")
+            self.puns[member.id] = {"count": 1}
+        await ctx.send(
+            f"{member.mention} has made {self.puns[member.id]['count']} \
+            awful jokes."
+        )
 
     @commands.command()
     async def subtract(self, ctx, *, member: discord.Member = None):
         member = member or ctx.author
         try:
-            self.puns[member.id]['count'] -= 1
-            await ctx.send(f"{member.mention}\'s last joke wasn\'t that bad. They\'ve \
-                made {self.puns[member.id]['count']} puns.")
+            self.puns[member.id]["count"] -= 1
+            await ctx.send(
+                f"{member.mention}'s last joke wasn't that bad. They've \
+                made {self.puns[member.id]['count']} puns."
+            )
         except KeyError:
-            await ctx.send(f'{member.mention} hasn\'t made any bad jokes,\
-                 unbelievably.')
+            await ctx.send(
+                f"{member.mention} hasn't made any bad jokes,\
+                 unbelievably."
+            )
 
 
 bot.add_cog(RecordPuns(bot))
