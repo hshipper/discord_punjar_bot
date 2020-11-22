1121032c0eb6b6e9ac7b8f1fe5bba6aab98d66b8
Henry Shipper <hshipper@gmail.com>
2020-11-22 11:34:11 -0800

    timezone set

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ import discord
 from discord.ext import commands
 from google.cloud import firestore
 from firestore_helpers import create_user_documents
+import os
 
+os.environ["TZ"] = "US/Western"
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="$", intents=intents)
@@ -15,7 +17,7 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is connected!")
-    await bot.change_presence(activity=discord.Game(name="around with tests"))
+    await bot.change_presence(activity=discord.Game(name="tracking your bad jokes"))
 
 
 class RecordPuns(commands.Cog):
@@ -36,8 +38,7 @@ class RecordPuns(commands.Cog):
         punmaker_data = self.db.collection("puns").document(str(member.id))
         pun_count = punmaker_data.get().to_dict()["pun_count"]
         punmaker_data.update({"pun_count": firestore.Increment(1)})
-        await ctx.send(f"{member.mention} has made {pun_count + 1} "
-                       "awful jokes.")
+        await ctx.send(f"{member.mention} has made {pun_count + 1} " "awful jokes.")
 
     @commands.command(name="subtract")
     async def subtract(self, ctx, *, member: discord.Member = None):
@@ -60,18 +61,15 @@ class RecordPuns(commands.Cog):
         member = member or ctx.author
         punmaker_data = self.db.collection("puns").document(str(member.id))
         pun_count = punmaker_data.get().to_dict()["pun_count"]
-        await ctx.send(f"{member.mention} has contributed ${pun_count}"
-                       " to the pun jar.")
+        await ctx.send(
+            f"{member.mention} has contributed ${pun_count}" " to the pun jar."
+        )
 
-
-"""
     @commands.command(name="lastpun")
     async def identify_last_pun(self, ctx, member: discord.Member = None):
         if member:
             punmaker_data = self.db.collection("puns").document(str(member.id))
             last_pun_time = punmaker_data.get().to_dict()["last_pun"]
-            last_pun_time_tz = last_pun_time.replace(tzinfo=timezone.pst
-            ).astimezone(tz=None)
             await ctx.send(
                 f"{member.mention} last made a pun at "
                 f"{last_pun_time.isoformat(sep=' ')}"
@@ -80,7 +78,7 @@ class RecordPuns(commands.Cog):
             await ctx.send(f"{self._last_punmaker.mention} made the last pun")
         else:
             await ctx.send("I forgot who made the last pun.")
-"""
+
 
 bot.add_cog(RecordPuns(bot))
 
--- firestore_helpers.py
+++ firestore_helpers.py
@@ -1,23 +1,30 @@
-'''
+"""
 functions for collating data to write to firestore
 mostly needed for initial setup
-'''
+"""
 import datetime
+import os
 
 # make a document for each member in the channel if they don't have one yet
 
 
 def create_user_documents(self):
+    os.environ["TZ"] = "US/Western"
     user_ids = [user.id for user in self.bot.users if not user.bot]
     string_user_ids = [str(user_id) for user_id in user_ids]
     batch = self.db.batch()
     for user in string_user_ids:
-        user_doc = self.db.collection(u'puns').document(user).get()
+        user_doc = self.db.collection("puns").document(user).get()
         if not user_doc.exists:
-            print(f'{user} needs to be added to database')
-            ref = self.db.collection(u'puns').document(user)
-            batch.set(ref, {u'date_added': datetime.datetime.now(),
-                            u'pun_count': 0,
-                            u'last_pun': None})
-    print('All new users have been added!')
+            print(f"{user} needs to be added to database")
+            ref = self.db.collection("puns").document(user)
+            batch.set(
+                ref,
+                {
+                    "date_added": datetime.datetime.now(),
+                    "pun_count": 0,
+                    "last_pun": None,
+                },
+            )
+    print("All new users have been added!")
     batch.commit()
