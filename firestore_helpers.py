5f79300f987738003a450e9ab7e45ea174eb9160
Henry Shipper <hshipper@gmail.com>
2020-11-22 10:16:30 -0800

    fix firestore helpers

--- firestore_helpers.py
+++ firestore_helpers.py
@@ -12,12 +12,8 @@ def create_user_documents(self):
     string_user_ids = [str(user_id) for user_id in user_ids]
     batch = self.db.batch()
     for user in string_user_ids:
-        query = self.db.collection(u'puns').where(u'id', u'==', user)
-        results = query.stream()
-        record_count = 0
-        for result in results:
-            record_count += 1
-        if record_count == 0:
+        user_doc = self.db.collection(u'puns').document(user).get()
+        if not user_doc.exists:
             print(f'{user} needs to be added to database')
             ref = self.db.collection(u'puns').document(user)
             batch.set(ref, {u'date_added': datetime.datetime.now(),
